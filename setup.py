2bbd9f364ea657ef2c6e260faef7557651f5e2fc
David Horsley <david.horsley@hydro.com.au>
2022-05-10 15:51:59 +1000

    bump version

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bayesian-optimization',
-    version='1.2.0-dev0',
-    url='https://github.com/fmfn/BayesianOptimization',
+    name="bayesian-optimization",
+    version="1.2.1-dev0",
+    url="https://github.com/fmfn/BayesianOptimization",
     packages=find_packages(),
-    author='Fernando Nogueira',
+    author="Fernando Nogueira",
     author_email="fmfnogueira@gmail.com",
-    description='Bayesian Optimization package',
-    long_description='A Python implementation of global optimization with gaussian processes.',
-    download_url='https://github.com/fmfn/BayesianOptimization/tarball/0.6',
+    description="Bayesian Optimization package",
+    long_description="A Python implementation of global optimization with gaussian processes.",
+    download_url="https://github.com/fmfn/BayesianOptimization/tarball/0.6",
     install_requires=[
         "numpy >= 1.9.0",
         "scipy >= 0.14.0",
         "scikit-learn >= 0.18.0",
     ],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-    ]
+        "License :: OSI Approved :: MIT License",
+    ],
 )
